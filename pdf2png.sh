9017a2a7cb7df3a77d8b5b68fa2763257cb35db5
Zhihua Ban <sawpara@126.com>
2017-03-05 18:54:47 +0800

    pdf2png

--- pdf2png.sh
+++ pdf2png.sh
@@ -1,13 +1,19 @@
 
 pdffile=$1
-
 pngfile=$2
 
+pdfname=`echo ${pdffile} | awk -F\/ '{print $NF}'` 
+pngname=`echo ${pngfile} | awk -F\/ '{print $NF}'`
+
+echo ${pdfname}
+echo ${pngname}
+
+echo ${pdffile}
 
 scp ${pdffile} aban@115.156.232.111:~/
 
-ssh aban@115.156.232.111 convert -verbose -density 150 -trim ${pdffile} -quality 100 -flatten -sharpen 0x1.0 ${pngfile}
-scp aban@115.156.232.111:~/${pngfile} .
-ssh aban@115.156.232.111 rm ${pdffile} ${pngfile}
+ssh aban@115.156.232.111 convert -verbose -density 150 -trim ${pdfname} -quality 100 -flatten -sharpen 0x1.0 ${pngname}
+scp aban@115.156.232.111:~/${pngname} ${pngfile}
+ssh aban@115.156.232.111 rm ${pdfname} ${pngname}
 
 

fbcb61af15e0f18c3ef96e1cf00ea5608dd4106f
Zhihua Ban <sawpara@126.com>
2017-03-05 18:58:24 +0800

    added file

---
+++ temp/pdf2png.sh
@@ -0,0 +1,12 @@
+
+pdffile=$1
+pngfile=$2
+
+
+scp ${pdffile} aban@115.156.232.111:~/
+
+ssh aban@115.156.232.111 convert -verbose -density 150 -trim ${pdffile} -quality 100 -flatten -sharpen 0x1.0 ${pngfile}
+scp aban@115.156.232.111:~/${pngfile} ${pngfile}
+ssh aban@115.156.232.111 rm ${pdffile} ${pngfile}
+
+
